884b36943a904ff435ab6d8fabe0e4f275b671c7
Sasmi <shehanawijerathne2@gmail.com>
2022-06-07 16:15:40 +0530

    updated opd jsx,css,ReceptionHome

---
+++ management-ui/src/views/Reception/OPD.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import "./opd.css"
+
+const OPD = () => {
+    return(
+        <div className="main__container">
+            <form className="form">
+
+                <div className="common__info">
+                    <h1>Applicant Details</h1>
+                    <div className="common__info__wrap">
+                        <div className="common__info__details">
+                            <div className="row">
+                                <div className="col">
+                                    <label htmlFor="id">ID</label>
+                                    <input className="form-control" type="text" id="id" name="id"/>
+                                </div>
+                                <div className="col">
+                                    <label htmlFor="date">Register Date</label>
+                                    <input className="form-control" type="date" id="date" name="date"/>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-2">
+                                    <label htmlFor="name">Title</label>
+                                    <select className="form-select form-select__title">
+                                        <option selected></option>
+                                        <option value="1">Mr</option>
+                                        <option value="2">Mrs</option>
+                                        <option value="3">Ms</option>
+                                    </select>
+                                </div>
+                                <div className="col">
+                                    <label htmlFor="name">Full Name</label>
+                                    <input className="form-control" type="text" id="name" name="name"/>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label htmlFor="date">Date of Birth</label>
+                                    <input className="form-control" type="date" id="dob" name="dob"/>
+                                </div>
+                                <div className="col">
+                                    <label htmlFor="mobile">Mobile No</label>
+                                    <input className="form-control" type="text" id="mobile" name="mobile"/>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="common__info__image">
+                            <img src={require("./images/1.png")} alt="" width="200px" height="200px"/>
+                        </div>
+
+                    </div>
+                </div>
+                <br/>
+                <div className="common__info">
+                    <div className="common__info__wrap">
+                        <div className="common__info__details">
+                            <div className="row">
+                                <div className="col">
+                                    <label htmlFor="address">Address</label>
+                                    <input className="form-control" type="text" id="address" name="address"/>
+                                </div>
+
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label htmlFor="phone">Phone</label>
+                                    <input className="form-control" type="phone" id="phone" name="phone"/>
+                                </div>
+
+                            </div>
+
+                            <div className="row">
+                                <div className="col">
+                                    <label htmlFor="lastmeal">Time of last meal</label>
+                                    <input className="form-control" type="text" id="lastmeal" name="lastmeal"/>
+                                </div>
+
+                                <div className="col">
+                                    <label htmlFor="country">Referred by</label>
+                                    <select className="form-select">
+                                        <option selected></option>
+                                        <option value="1">Mr.Rohan</option>
+                                        <option value="2">Mario</option>
+                                        <option value="3">Nurse Anty</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <button type="button" className="btn btn-primary">Save</button>
+
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default OPD
