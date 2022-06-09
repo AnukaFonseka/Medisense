717b887a21af0010adb746f43b14cf2a606b73d4
Yahampath <yhmpth@gmail.com>
2022-06-09 19:31:11 +0530

    receptionHome.css edited

--- management-ui/src/views/App.js
+++ management-ui/src/views/App.js
@@ -6,7 +6,6 @@ import GCC from "./Reception/GCC"
 import {ConnectedRouter} from "connected-react-router";
 import {history} from "../router/router";
 import OPD from "./Reception/OPD";
-import TestDetails from "./Reception/TestDetails";
 
 function App() {
 
@@ -18,7 +17,6 @@ function App() {
                     <Route exact path="/receptionHome" component={ReceptionHome}></Route>
                     <Route exact path="/GCC" component={GCC}></Route>
                     <Route exact path="/OPD" component={OPD}></Route>
-                    <Route exact path="/testDetails" component={TestDetails}></Route>
                     <Redirect exact to="/login" from="/"/>
                 </Switch>
             </ConnectedRouter>
