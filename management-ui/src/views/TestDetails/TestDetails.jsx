249a1479670b0325a8f7de5f5d02590a24de0712
Yahampath <yhmpth@gmail.com>
2022-06-15 17:19:01 +0530

    CashierList.jsx created

--- management-ui/src/views/TestDetails/TestDetails.jsx
+++ management-ui/src/views/TestDetails/TestDetails.jsx
@@ -102,7 +102,7 @@ const TestDetails = () => {
                             </tbody>
                         </table>
 
-                        <NavLink to = "/testDetails" className = "btn btn-primary mb-2" > Save </NavLink>
+                        <NavLink to = "/cashierList" className = "btn btn-primary mb-2" > Save </NavLink>
 
                     </div>
 
