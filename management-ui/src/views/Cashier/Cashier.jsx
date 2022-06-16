2f125a47fdab1599eff2c252bee2dfe055ddf631
Sasmi <shehanawijerathne2@gmail.com>
2022-06-17 00:03:30 +0530

    updated cashier.css,Cashier.jsx

--- management-ui/src/views/Cashier/Cashier.jsx
+++ management-ui/src/views/Cashier/Cashier.jsx
@@ -73,92 +73,85 @@ const Cashier = () => {
                 <div className="common__info">
                     <div className="common__info__wrap">
                         <div className="common__info__details">
-                            <div className="row">
-                                <div className="col">
-                                    <label htmlFor="commission">Commission</label>
-                                    <input className="form-control" type="text" id="commission" name="commission"/>
-                                </div>
-                                <div className="col">
-                                    <label htmlFor="total">Total</label>
-                                    <input className="form-control" type="text" id="total" name="total"/>
+                                <div className="row">
+                                    <div className="col">
+                                        <label htmlFor="commission">Commission</label>
+                                        <input className="form-control" type="text" id="commission" name="commission"/>
+                                    </div>
+                                    <div className="col">
+                                        <label htmlFor="total">Total</label>
+                                        <input className="form-control" type="text" id="total" name="total"/>
+                                    </div>
                                 </div>
 
-                            </div>
-
-                            <div className="row">
-                                <div className="col">
-                                    <label htmlFor="discount">Discount</label>
-                                    <input className="form-control" type="text" id="discount" name="discount"/>
+                                <div className="row">
+                                    <div className="col">
+                                        <label htmlFor="discount">Discount</label>
+                                        <input className="form-control" type="text" id="discount" name="discount"/>
+                                    </div>
+                                    <div className="col">
+                                        <label htmlFor="amountPay">Amount to Pay</label>
+                                        <input className="form-control" type="text" id="amountPay" name="amountPay"/>
+                                    </div>
                                 </div>
-                                <div className="col">
-                                    <label htmlFor="amountPay">Amount to Pay</label>
-                                    <input className="form-control" type="text" id="amountPay" name="amountPay"/>
-                                </div>
-                            </div>
 
 
-                            <div className="category_container">
-                                <div className="form--check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
-                                    <label className="form-check-label" htmlFor="flexCheckDefault">
-                                        Cash
-                                    </label>
-                                </div>
-                                <div className="form--check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                                    <label className="form-check-label" htmlFor="flexCheckDefault">
-                                        Cheque
-                                    </label>
-                                </div>
-                                <div className="form--check">
-                                    <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
-                                    <label className="form-check-label" htmlFor="flexCheckDefault">
-                                        Credit
-                                    </label>
-                                </div>
-                            </div>
+                                <div className="category_container">
+                                    <div className="form--check">
+                                        <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" />
+                                        <label className="form-check-label" htmlFor="flexCheckDefault">
+                                            Cash
+                                        </label>
+                                    </div>
 
-                            <div className="row">
-                                <div className="col">
-                                    <label htmlFor="cheque">Cheque#</label>
-                                    <input className="form-control" type="text" id="cheque" name="cheque"/>
-                                </div>
-                                <div className="col">
-                                    <label htmlFor="country">Credit Approved By</label>
-                                    <select className="form-select">
-                                        <option selected></option>
-                                        <option value="1">Mr.Rohan</option>
-                                        <option value="2">Mario</option>
-                                        <option value="3">Nurse Anty</option>
-                                    </select>
-                                </div>
-                            </div>
+                                    <div className="category_con">
+                                        <div className="form--check">
+                                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                                            <label className="form-check-label" htmlFor="flexCheckDefault">
+                                                Cheque
+                                            </label>
+                                        </div>
 
-                            <div className="row">
-                                <div className="col_box">
-                                    <label htmlFor="chequeDate">Cheque Date</label>
-                                    <input className="form-control" type="text" id="chequeDate" name="chequeDate"/>
+                                        <div  className="category-box">
+                                            <div className="row">
+                                                <input className="form-ctrl" type="text" id="cheque" name="cheque" placeholder="Cheque No"/>
+                                            </div>
+                                            <div className="row">
+                                                <input className="form-ctrl" type="text" id="chequeDate" name="chequeDate" placeholder="Cheque Date"/>
+                                            </div>
+                                            <div className="row">
+                                                <select className="form-select select__bank" >
+                                                    <option selected></option>
+                                                    <option value="" disabled selected>Bank</option>
+                                                    <option value="1">BOC</option>
+                                                    <option value="2">Seylan</option>
+                                                    <option value="3">NDB</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="category_con">
+                                        <div className="form--check">
+                                            <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3"/>
+                                            <label className="form-check-label" htmlFor="flexCheckDefault">
+                                                Credit
+                                            </label>
+                                        </div>
+                                        <div className="col">
+                                            <select className="form-select">
+                                                <option value="" disabled selected>Credit Approved By</option>
+                                                <option value="1">Mr.Rohan</option>
+                                                <option value="2">Mario</option>
+                                                <option value="3">Nurse Anty</option>
+                                            </select>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
 
-                            <div className="row">
-                                <div className="col_box">
-                                    <label htmlFor="bank">Bank</label>
-                                    {/*<div className="bankDiv">*/}
-                                        <select className="form-select select__bank">
-                                            <option selected></option>
-                                            <option value="1">BOC</option>
-                                            <option value="2">Seylan</option>
-                                            <option value="3">NDB</option>
-                                        </select>
-                                    {/*</div>*/}
+                                <div className="button">
+                                        <NavLink to = "/testDetails" className = "btn btn-primary mb-2" > Save </NavLink>
+                                        <NavLink to = "/testDetails" className = "btn btn-primary mb-2" > Print </NavLink>
                                 </div>
-                            </div>
-
-                            <div className="button">
-                                    <NavLink to = "/testDetails" className = "btn btn-primary mb-2" > Save </NavLink>
-                                    <NavLink to = "/testDetails" className = "btn btn-primary mb-2" > Print </NavLink>
-                            </div>
                         </div>
                     </div>
                 </div>
